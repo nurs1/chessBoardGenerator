fdad181cb44f6d19e659b193ec593c4d01ae1964
nurs1 <nursultan1201b@gmail.com>
2017-04-03 14:06:42 +0600

    fixes of possible bugs (var -> let)
    node with latest versions, since let operator

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 function myChess(n){
-	var desk = new Array(n);
+	let desk = new Array(n);
 	for(let i = 0; i < n; i += 1){
 		desk[i] = new Array(n);
 	}
-	var w = 0
-	for(var i = 0; i < n; i += 1){
-		for(var j = 0; j < n; j += 1){
+	let w = 0
+	for(let i = 0; i < n; i += 1){
+		for(let j = 0; j < n; j += 1){
 			if(j == 0){
 				if(i % 2 == 0){
 					desk[i][j] = 0;
@@ -21,8 +21,8 @@ function myChess(n){
 			w ^= 1;
 		}
 	}
-	var res = '';
-	for(var i = 0; i < n; i += 1){
+	let res = '';
+	for(let i = 0; i < n; i += 1){
 		res += desk[i];
 		res += '\n';
 	}
